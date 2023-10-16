d9d11317eddb85047e10e081f181e36fb989f219
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2023-10-16 17:51:52 +0200

    css changes

--- src/components/organisms/Navbar/Navbar.jsx
+++ src/components/organisms/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
-import {NavLink} from "react-router-dom";
-import style from "./Navbar.module.scss"
+import { NavLink } from "react-router-dom";
+import style from "./Navbar.module.scss";
 const Navbar = () => {
-    return (
-        <nav className={style.nav}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-        </nav>
-    );
+  return (
+    <nav className={style.nav}>
+      <NavLink to="/" className={style.navlink}>
+        Home
+      </NavLink>
+      <NavLink to="/about" className={style.navlink}>
+        About
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navbar;
